bb9a1bbfccdf015ab09bd9bf3b679b9b62493578
hannesivask <hannesegmond@gmail.com>
2024-03-20 09:12:30 +0200

    Update: updated front end

--- public/js/index.js
+++ public/js/index.js
@@ -5,11 +5,14 @@ import 'regenerator-runtime/runtime.js';
 
 import { displayMap } from './mapbox.js';
 import { login, logout } from './login.js';
+import { updateSettings } from './updateSettings.js';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const updateDataForm = document.querySelector('.form-user-data');
+const updatePasswordForm = document.querySelector('.form-user-password');
 
 // DELEGATION
 if (mapBox) {
@@ -30,4 +33,37 @@ if (loginForm) {
 
 if (logoutBtn) {
   logoutBtn.addEventListener('click', logout);
+}
+
+if (updateDataForm) {
+  updateDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (updatePasswordForm) {
+  updatePasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password',
+    );
+
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+  });
 }
---
+++ public/js/updateSettings.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+// type is either password or data
+export const updateSettings = async (data, type) => {
+  try {
+    const res = await axios({
+      method: 'PATCH',
+      url: `http://127.0.0.1:3000/api/v1/users/${type === 'password' ? 'updateMyPassword' : 'updateMe'}`,
+      data,
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', `${type.toUpperCase()} updated successfully`);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
