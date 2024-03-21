fc34feff8936678a230b92faeafe409e8c3239b6
hannesivask <hannesegmond@gmail.com>
2024-03-21 12:59:50 +0200

    git

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -14,7 +14,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
 
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/myTours`,
     // success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
 
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
