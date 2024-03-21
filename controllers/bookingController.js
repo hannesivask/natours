543485d7794c5dcd5c22f4e2b410d2c0d019c885
hannesivask <hannesegmond@gmail.com>
2024-03-21 14:11:32 +0200

    Update stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -28,7 +28,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     success_url: `${req.protocol}://${req.get('host')}/myTours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
-    client_reference_id: req.params.tourID,
+    client_reference_id: req.params.tourId,
     mode: 'payment',
     line_items: [
       {
